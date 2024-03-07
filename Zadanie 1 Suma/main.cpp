6a65bc017cb2281fb4571137d701f11a16ad0398
cichy-michal <156360877+cichy-michal@users.noreply.github.com>
2024-03-07 15:25:39 +0100

    Add files via upload

---
+++ Zadanie 1 Suma/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a,b,suma;
+    cout<<"Wpisz 1 liczbe:"<<endl;
+    cin>>a;
+    cout<<"Wpisz 2 liczbe:"<<endl;
+    cin>>b;
+    suma=a+b;
+    cout<<"Suma twoich liczb wynosi:"<<suma<<endl;
+    return 0;
+
+}
